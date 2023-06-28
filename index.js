00c5cce64473f628d3499c4ab039f3049d43281b
Naganath P <137897192+NaganathP@users.noreply.github.com>
2023-06-28 19:39:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import Header from "./Header.js"
+import MainContent from "./MainContent.js"
+import Footer from "./Footer.js"
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+ReactDOM.render(<Page />, document.getElementById("root"))
